169c020804e92a89a75f9d5c17feea60f124674a
Michelle Osorio <Osoriomica@gmail.com>
2025-02-05 22:07:13 +0000

    Fix: Add couple of checks before and after updating currentFlashIndex to avoid triggering a console error

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -31,14 +31,14 @@ shooterOptions.forEach(option => {
     option.addEventListener('click', chooseShooter)
 })
 
-function chooseShooter(e){
+function chooseShooter(e) {
     const selectedShooter = e.target
     console.log('Chosen shooter:', selectedShooter.id)
 
     shooter.classList = ''
-    
-    for (const className of selectedShooter.classList){
-        shooter.classList.add(className) 
+
+    for (const className of selectedShooter.classList) {
+        shooter.classList.add(className)
         //this code passes each class individually to .shooter as the classList.add() method was seeing the classes (.shooter .shooterN)as a single class name and returning an InvalidCharacterError
     }
 }
@@ -52,7 +52,7 @@ const soundsList = {
 Object.values(soundsList).forEach(sound => sound.volume = 0.2)
 
 // toggle volume for every sound in soundsList
-function toggleSound(){
+function toggleSound() {
     const newVolume = (toggleSoundButton.classList.contains('sound-on')) ? 0 : 0.2
 
     // set volume for each sound in soundsList
@@ -61,7 +61,7 @@ function toggleSound(){
     // toggle sound button background images
     toggleSoundButton.classList.toggle('sound-on')
     toggleSoundButton.classList.toggle('sound-off')
-    
+
 }
 
 //Original toggle sound function targetting only one Audio
@@ -163,18 +163,19 @@ function moveInvaders() {
         alienInvaders[i] += direction
     }
     draw()
-// original code by Ania Kubow, modified to address bug: game would not end unless invaders actually collided with current shooter index so they could pass by if there was a gap in the middle. 
+    // original code by Ania Kubow, modified to address bug: game would not end unless invaders actually collided with current shooter index so they could pass by if there was a gap in the middle. 
     if (squares[currentShooterIndex].classList.contains('invader')) {
         resultDisplay.innerHTML = `<h4>Game Over</h4> You shot ${score} invaders`
-        soundsList.gameOverSFX.play() 
+        soundsList.gameOverSFX.play()
         clearInterval(invadersId)
         squares[currentShooterIndex].classList.remove('camera') //removes shooter from grid on game over
-        setTimeout(() =>remove(), 500) //removes all invaders on game Over
-    }
+        setTimeout(() => remove(), 500) //removes all invaders on game Over
+    } 
     if (invadersRemoved.length === alienInvaders.length) {
         resultDisplay.innerHTML = `<h4>You Win!</h4> You shot ${score} invaders`
         clearInterval(invadersId)
     }
+    return
 }
 
 // Shoot function - original code by Ania Kubow. Edited to customise game
@@ -185,26 +186,28 @@ function shoot(e) {
     function moveFlash() {
         console.log(squares.length)
         console.log(currentFlashIndex)
-
-        if(currentFlashIndex <0 || currentFlashIndex >= squares.length){
-            clearInterval(flashId)
-            return
-        }
         
-        squares[currentFlashIndex].classList.remove('flash')
-        currentFlashIndex -= width
-
-        if(currentFlashIndex <0 || currentFlashIndex >= squares.length){
-            clearInterval(flashId)
-            return
+        // ensure currentFlashIndex is within bounds of the grid
+        if ((currentFlashIndex < 0 || currentFlashIndex >= squares.length) ) {
+            clearInterval(flashId) // stop the flash if it goes out of bounds (
+            return // Exit the function if the index is out of bounds
         }
+
+        squares[currentFlashIndex].classList.remove('flash')
+        currentFlashIndex -= width   //This makes the flash go upwards ie: 217-15 = 202 (directly above the currentFlashIndex that triggers the flash)
+        
+        if ((currentFlashIndex < 0 || currentFlashIndex >= squares.length) ) {
+            clearInterval(flashId) // stop the flash if it goes out of bounds on its way up
+            return // Exit the function if the index is out of bounds
+        }
+
         squares[currentFlashIndex].classList.add('flash')
 
         if (squares[currentFlashIndex].classList.contains('invader')) {
             squares[currentFlashIndex].classList.remove('flash')
             squares[currentFlashIndex].classList.remove('invader')
             squares[currentFlashIndex].classList.add('boom')
-            
+
 
             setTimeout(() => squares[currentFlashIndex].classList.remove('boom'), 300) //makes .boom disappear after 300 miliseconds
             clearInterval(flashId)
@@ -219,13 +222,13 @@ function shoot(e) {
     if (e.key === 'ArrowUp' || e.key === " " || e.target.id === 'shooter') {
         e.preventDefault()
         flashId = setInterval(moveFlash, 100)
-        soundsList.boomSFX.play() 
+        soundsList.boomSFX.play()
     }
 }
 
 // show modal on page load 
 window.onload = () => {
-    setTimeout(() =>{
+    setTimeout(() => {
         gameModal.show()
     }, 300)
 }
@@ -247,8 +250,8 @@ document.addEventListener('keydown', shoot)
 shooter.addEventListener('click', shoot)
 
 // Pause/Resume button
-function togglePauseResume(e){
-    if (invadersId){
+function togglePauseResume(e) {
+    if (invadersId) {
         //Game is running from the beginning so we need to pause it first
         clearInterval(invadersId)
         invadersId = ""
@@ -263,7 +266,7 @@ function togglePauseResume(e){
 startPause.addEventListener('click', togglePauseResume)
 
 // Restart button - resets game state and starts over
-function restartGame(e){
+function restartGame(e) {
     location.reload()
 }
 
